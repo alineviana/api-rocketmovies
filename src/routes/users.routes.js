87b293135321c7aad8f6d726af692cda08720179
Aline Cristina Viana da Costa <alinecristinaviana@gmail.com>
2023-02-09 20:44:43 -0300

    add files

---
+++ src/routes/users.routes.js
@@ -0,0 +1,11 @@
+const { Router } = require("express");
+
+const usersRouter = Router();
+
+const UsersController = require("../controllers/UsersController");
+
+const usersController = new UsersController();
+
+usersRouter.post("/", usersController.create);
+
+module.exports = usersRouter;
